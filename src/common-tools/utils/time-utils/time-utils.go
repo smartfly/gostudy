f14204c153665d19d43908a590bdcd68b61251a4
smartfly <hujitao2010@163.com>
2019-05-10 10:31:01 +0800

    新增时间转化工具

---
+++ src/common-tools/utils/time-utils/time-utils.go
@@ -0,0 +1,39 @@
+package time_utils
+
+import "time"
+
+const timeLayout = "2006-01-02 15:04:05"
+
+// Time 时间转化为string
+func Time2Str(time time.Time) string {
+	timeString := time.Format(timeLayout)
+	return timeString
+}
+
+// 时间戳timestamp转string
+func Timestamp2Str(timestamp int64) string {
+	timeValue := time.Unix(timestamp, 0)
+	timeString := Time2Str(timeValue)
+	return timeString
+}
+
+// string转时间time
+func Str2Time(timeStr string) (time.Time, error) {
+	loc, _ := time.LoadLocation("Local")
+	// 使用模板在对应时区转化为time.time类型
+	theTime, err := time.ParseInLocation(timeLayout, timeStr, loc)
+	if err != nil {
+		return time.Time{}, err
+	}
+	return theTime, nil
+}
+
+// string转时间戳timestamp
+func Str2Timestamp(timeStr string) (int64, error) {
+	theTime, err := Str2Time(timeStr)
+	if err != nil {
+		return 0, err
+	}
+	timestamp := theTime.Unix()
+	return timestamp, nil
+}
---
+++ src/common-tools/utils/time-utils/time-utils_test.go
@@ -0,0 +1,27 @@
+package time_utils
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestTime2Str(t *testing.T) {
+	result := Time2Str(time.Now())
+	fmt.Printf("time to time string: %s\n", result)
+}
+
+func TestTimestamp2Str(t *testing.T) {
+	result := Timestamp2Str(time.Now().Unix())
+	fmt.Printf("timestamp to time string: %s\n", result)
+}
+
+func TestStr2Time(t *testing.T) {
+	result, _ := Str2Time("2019-05-10 10:26:36")
+	fmt.Printf(" time string to time: %s\n", result)
+}
+
+func TestStr2Timestamp(t *testing.T) {
+	result, _ := Str2Timestamp("2019-05-10 10:26:36")
+	fmt.Printf(" time string to timestamp: %d\n", result)
+}
