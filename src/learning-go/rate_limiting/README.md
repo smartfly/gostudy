8f5098bb3373934e918424f319ad91696ae0f6c7
smartfly <hujitao2010@163.com>
2019-09-18 10:50:38 +0800

    golang rate limit使用

---
+++ src/learning-go/rate_limiting/README.md
@@ -0,0 +1,5 @@
+
+# 参考资料
+[Go如何实现HTTP请求限流](https://xiequan.info/go%E5%A6%82%E4%BD%95%E5%AE%9E%E7%8E%B0http%E8%AF%B7%E6%B1%82%E9%99%90%E6%B5%81/)
+
+[Rate Limiting](https://github.com/golang/go/wiki/RateLimiting)
---
+++ src/learning-go/rate_limiting/rate_limit_test.go
@@ -0,0 +1,63 @@
+package rate_limiting
+
+import (
+	"context"
+	"fmt"
+	"golang.org/x/time/rate"
+	"net/http"
+	"testing"
+	"time"
+)
+
+/*
+ * @author taohu
+ * @date 2019/9/17
+ * @time 下午3:54
+ * @desc 限流demo
+**/
+// r：每秒发生多少次事件, b：其缓存最大可存多少个事件
+var limiter = rate.NewLimiter(2, 5)
+
+func limit(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if limiter.Allow() == false {
+			http.Error(w, http.StatusText(429), http.StatusTooManyRequests)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func TestRateLimitByTenQPS(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", okHandler)
+
+	// warp the server mux with the limit middleware
+	http.ListenAndServe(":4000", limit(mux))
+}
+
+func okHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("ok"))
+}
+
+func TestLimiter(t *testing.T) {
+	l := rate.NewLimiter(2, 5)
+	ctx := context.Background()
+	start := time.Now()
+	// 要处理二十个事件
+	for i := 0; i < 20; i++ {
+		l.Wait(ctx)
+		fmt.Println(l.Limit(), l.Burst())
+	}
+	fmt.Println(time.Since(start))
+}
+
+func TestReserve(t *testing.T) {
+	l := rate.NewLimiter(1, 3)
+	for {
+		r := l.ReserveN(time.Now(), 3)
+		fmt.Println(r.Delay())
+		fmt.Println(time.Now().Format("2016-01-02 15:04:05.000"))
+	}
+}
