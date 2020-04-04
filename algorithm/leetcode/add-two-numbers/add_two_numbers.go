164910f1ee85d59d8443355dd61825de956b3ffa
smartfly <hujitao2010@163.com>
2020-04-04 10:17:11 +0800

    golang leetcode add two numbers recursive solution

--- algorithm/leetcode/add-two-numbers/add_two_numbers.go
+++ algorithm/leetcode/add-two-numbers/add_two_numbers.go
@@ -17,6 +17,7 @@ type ListNode struct {
 	Next *ListNode
 }
 
+// addTwoNumbers 循环迭代法
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	result := &ListNode{
 		Val: 0,
@@ -54,4 +55,37 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		}
 	}
 	return result.Next
+}
+
+// addTwoNumbersByRecursive 递归的解法
+func addTwoNumbersByRecursive(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil && l2 == nil {
+		return nil
+	}
+
+	if l1 == nil {
+		return l2
+	}
+
+	if l2 == nil {
+		return l1
+	}
+
+	sum := l1.Val + l2.Val
+	nextNode := addTwoNumbersByRecursive(l1.Next, l2.Next)
+	if sum < 10 {
+		return &ListNode{
+			Val:  sum,
+			Next: nextNode,
+		}
+	} else {
+		tempNode := &ListNode{
+			Val:  1,
+			Next: nil,
+		}
+		return &ListNode{
+			Val:  sum - 10,
+			Next: addTwoNumbersByRecursive(nextNode, tempNode),
+		}
+	}
 }
