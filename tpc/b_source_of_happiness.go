413d245f9199d0f3155d59b2c24ac6763297ff49
smartfly <hujitao2010@163.com>
2020-08-04 21:57:58 +0800

    golang tpc算法

---
+++ tpc/b_source_of_happiness.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+ * @author taohu
+ * @date 2020/8/4
+ * @time 下午7:16
+ * @desc please describe function
+**/
+
+func main() {
+	var n int
+	_, _ = fmt.Scanln(&n)
+	for i := 0; i < n; i++ {
+		var total, sum int
+		_, _ = fmt.Scanln(&total, &sum)
+		nums := make([]int, total)
+		var temp int
+		for i := 0; i < total; i++ {
+			_, _ = fmt.Scan(&temp)
+			nums[i] = temp
+		}
+
+		if total <= 2 {
+			fmt.Println("Impossible")
+			continue
+		} else {
+			fixNums := make([]int, total)
+			for i := 2; i < total; i++ {
+				tempSum := nums[i-2] + nums[i-1] + nums[i]
+				if tempSum < sum {
+					if nums[i-2] == -1 && nums[i-1] == -1 && nums[i] == -1 { // -1 -1 -1
+						fmt.Println("Impossible")
+						break
+					} else if nums[i-2] == -1 && nums[i-1] != -1 && nums[i] != -1 { // -1 0 -1
+						temp = sum - nums[i-1] - nums[i]
+						if temp > fixNums[i-2] {
+							fixNums[i-2] = temp
+						}
+					} else if nums[i-2] != -1 && nums[i-1] == -1 && nums[i] != -1 { // 0 -1 0
+						temp = sum - nums[i-2] - nums[i]
+						if temp > fixNums[i-1] {
+							fixNums[i-1] = temp
+						}
+					} else if nums[i-2] != -1 && nums[i-1] != -1 && nums[i] == -1 { // 0 0 -1
+						temp = sum - nums[i-2] - nums[i-1]
+						if temp > fixNums[i] {
+							fixNums[i] = temp
+						}
+					} else if nums[i-2] == -1 && nums[i-1] == -1 && nums[i] != -1 { // -1 -1 0
+						temp = sum - fixNums[i-2] - nums[i]
+						if temp > fixNums[i-1] {
+							fixNums[i-1] = temp
+						}
+					} else if nums[i-2] == -1 && nums[i-1] != -1 && nums[i] == -1 { // -1 0 -1
+						temp = sum - fixNums[i-2] - nums[i-1]
+						if temp > fixNums[i] {
+							fixNums[i] = temp
+						}
+					} else if nums[i-2] != -1 && nums[i-1] == -1 && nums[i] == -1 { // 0 0 -1
+						temp = sum - fixNums[i-2] - nums[i-1]
+						if temp > fixNums[i] {
+							fixNums[i] = temp
+						}
+					} else if nums[i-2] != -1 && nums[i-1] != -1 && nums[i] != -1 { // 0 0 0
+						if tempSum == 0 {
+							fmt.Println("Impossible")
+							break
+						}
+					}
+				}
+			}
+			var result int
+			for i := 0; i < total; i++ {
+				if nums[i] == -1 {
+					result += fixNums[i]
+				} else {
+					result += nums[i]
+				}
+			}
+			fmt.Println(result)
+		}
+	}
+}
---
+++ tpc/future_of_tencent.go
@@ -0,0 +1,28 @@
+package main
+
+/*
+ * @author taohu
+ * @date 2020/8/4
+ * @time 下午7:06
+ * @desc please describe function
+**/
+
+//func main() {
+//	var n int
+//	_, _ = fmt.Scanln(&n)
+//	nums := make([]int, n)
+//	var temp int
+//	for i := 0; i < n; i++ {
+//		_, _ = fmt.Scanln(&temp)
+//		nums[i] = temp
+//	}
+//
+//	for _, val := range nums {
+//		if val == 0 {
+//			fmt.Println("Good luck and Enjoy TPC!")
+//		} else {
+//			fmt.Println("You are the future of Tencent!")
+//		}
+//	}
+//
+//}
