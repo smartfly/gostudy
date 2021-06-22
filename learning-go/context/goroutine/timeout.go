3772ca9b6e59c721bce18299656a1167407d5d10
smartfly <hujitao2010@163.com>
2021-06-22 17:09:51 +0800

    golang ctx current timeout problem

---
+++ learning-go/context/goroutine/timeout.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+/**
+同一个context可以控制多个goroutine, 确保线程可控, 而不是每新建一个goroutine就要有一个chan去通知他关闭
+有了他代码更加简洁
+*/
+
+func main() {
+	fmt.Println("run demo \n\n\n")
+	demo()
+}
+
+func demo() {
+	ctx, cancel := context.WithTimeout(context.Background(), 9*time.Second)
+	go watch(ctx, "[线程1]", 10)
+	go watch(ctx, "[线程2]", 2)
+	//go watch(ctx, "[线程3]", 3)
+
+	index := 0
+	for {
+		index++
+		//fmt.Printf("%d 秒过去了 \n", index)
+		time.Sleep(5 * time.Second)
+		if index > 10 {
+			break
+		}
+	}
+
+	fmt.Println("通知停止监控")
+	// 其实此时已经超时, 协程已经提前退出
+	cancel()
+
+	// 防止主进程提前退出
+	time.Sleep(3 * time.Second)
+	fmt.Println("done")
+}
+
+func watch(ctx context.Context, name string, num int) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Printf("%v %s  监控退出, 停止了...\n", time.Now(), name)
+			return
+		default:
+			//fmt.Printf("%s goroutine监控中... \n", name)
+			fmt.Println(time.Now(), name, ctx)
+			fmt.Printf("%p\n", &ctx)
+			a := time.Duration(num)
+			time.Sleep(a * time.Second)
+		}
+	}
+}
