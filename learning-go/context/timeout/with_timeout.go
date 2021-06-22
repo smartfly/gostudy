3a6885e4e704c4b1386d344a3abf5861fe6ad07d
smartfly <hujitao2010@163.com>
2021-06-22 17:18:25 +0800

    golang ctx timeout setting

---
+++ learning-go/context/timeout/with_timeout.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	// Pass a context with a timeout to tell a blocking function that it
+	// should abandon its work after the timeout elapses.
+	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
+	defer cancel()
+	// select 用来监听和channel有关的IO操作, 当IO操作发生时, 触发响应的动作
+	select {
+	case <-time.After(1 * time.Second):
+		fmt.Println("overslept")
+	case <-ctx.Done():
+		fmt.Println(ctx.Err()) // prints "context deadline exceeded"
+	}
+
+}
